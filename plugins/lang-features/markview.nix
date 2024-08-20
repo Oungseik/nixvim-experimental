702b03c8109f5b9b644e0f96c143e08c1eb31252
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-21 00:45:50 +0630

    feat: add markview for markdown preview

---
+++ plugins/lang-features/markview.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+  plugins.markview.enable = true;
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<Leader>pm";
+      action = "<Nop>";
+      options = { silent = true; desc = "+Markdown"; };
+    }
+    {
+      mode = "n";
+      key = "<Leader>pmt";
+      action = ":Markview toggle<CR>";
+      options = { silent = true; desc = "+Markdown"; };
+    }
+  ];
+}
