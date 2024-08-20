660980ce2767d1b6f0acd121dbcad642686918ad
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-20 23:47:29 +0630

    feat: add frequently use keybindings inspired by lunarvim

--- plugins/core/keymaps.nix
+++ plugins/core/keymaps.nix
@@ -1,3 +1,81 @@
 {
   globals.mapleader = " ";
+
+  keymaps = [
+
+    # normal mode
+    {
+      mode = "n";
+      key = "<leader>q";
+      options.silent = true;
+      options.desc = "Quit";
+      action = "<cmd>q<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>w";
+      options.silent = true;
+      options.desc = "Save";
+      action = "<cmd>noa w<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>h";
+      options.silent = true;
+      options.desc = "No Highlight";
+      action = "<cmd>nohlsearch<CR>";
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      options.silent = true;
+      action = "<C-w>l";
+    }
+    {
+      mode = "n";
+      key = "<C-h>";
+      options.silent = true;
+      action = "<C-w>h";
+    }
+    {
+      mode = "n";
+      key = "<C-j>";
+      options.silent = true;
+      action = "<C-w>j";
+    }
+    {
+      mode = "n";
+      key = "<C-k>";
+      options.silent = true;
+      action = "<C-w>k";
+    }
+
+    {
+      mode = "n";
+      key = "<Leader>p";
+      action = "<Nop>";
+      options = { silent = true; desc = "+Plugins More"; };
+    }
+
+    # swapping lines
+    { mode = "n"; key = "<A-j>"; action = ":m .+1<CR>=="; }
+    { mode = "n"; key = "<A-k>"; action = ":m .-2<CR>=="; }
+
+    # insert mode bindings
+    {
+      mode = "i";
+      key = "<C-l>";
+      options.silent = true;
+      action = "<End>";
+    }
+
+    # visual mode bindings
+    # select lines and indent
+    { mode = "v"; key = ">"; action = ">gv"; }
+    { mode = "v"; key = "<"; action = "<gv"; }
+
+    # select lines and swap
+    { mode = "v"; key = "<A-j>"; action = ":m '>+1<CR>gv=gv"; }
+    { mode = "v"; key = "<A-k>"; action = ":m '<-2<CR>gv=gv"; }
+  ];
 }
