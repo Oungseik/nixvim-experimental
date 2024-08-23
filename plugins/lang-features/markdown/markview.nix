bec71b4e046fa5a7802b94b9f122b93f21c51b1d
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-23 13:15:33 +0630

    format the code

--- plugins/lang-features/markdown/markview.nix
+++ plugins/lang-features/markdown/markview.nix
@@ -6,13 +6,19 @@
       mode = "n";
       key = "<Leader>pm";
       action = "<Nop>";
-      options = { silent = true; desc = "+Markdown"; };
+      options = {
+        silent = true;
+        desc = "+Markdown";
+      };
     }
     {
       mode = "n";
       key = "<Leader>pmt";
       action = ":Markview toggle<CR>";
-      options = { silent = true; desc = "+Markdown"; };
+      options = {
+        silent = true;
+        desc = "+Markdown";
+      };
     }
   ];
 }
