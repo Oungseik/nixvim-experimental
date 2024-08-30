3f8b01aec392ec8c46bfd6ad7df30921bb30707e
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-30 07:25:51 +0630

    feat: update conform setting

--- plugins/lang-features/conform.nix
+++ plugins/lang-features/conform.nix
@@ -1,40 +1,83 @@
-{ pkgs, helpers, ... }: {
+{ pkgs, helpers, ... }:
+{
   plugins.conform-nvim = {
     enable = true;
-    notifyOnError = true;
 
-    formatters = {
-      stylua = {
-        command = "${pkgs.stylua}/bin/stylua";
+    settings = {
+      format_on_save = {
+        timeoutMs = 500;
+        lspFallback = true;
+
       };
-      nixfmt = {
-        command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+      notify_on_error = true;
+
+      formatters = {
+        stylua = {
+          command = "${pkgs.stylua}/bin/stylua";
+        };
+        nixfmt = {
+          command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+        };
+        biome = {
+          command = "${pkgs.biome}/bin/biome";
+          args = [
+            "--trailing-comma=es5"
+            "--semicolons=always"
+            "--javascript-formatter-enabled=true"
+            "--javascript-formatter-indent-style=space"
+          ];
+        };
+        black = {
+          command = "${pkgs.black}/bin/black";
+        };
       };
-      biome = {
-        command = "${pkgs.biome}/bin/biome";
-      };
-      black = {
-        command = "${pkgs.black}/bin/black";
+
+      formatters_by_ft = {
+        javascript =
+          (helpers.listToUnkeyedAttrs [
+            "prettierd"
+            "prettier"
+            "biome"
+          ])
+          // {
+            stopAfterFirst = true;
+          };
+        typescript =
+          (helpers.listToUnkeyedAttrs [
+            "prettierd"
+            "prettier"
+            "biome"
+          ])
+          // {
+            stopAfterFirst = true;
+          };
+        javascriptreact =
+          (helpers.listToUnkeyedAttrs [
+            "prettierd"
+            "prettier"
+            "biome"
+          ])
+          // {
+            stopAfterFirst = true;
+          };
+        typescriptreact =
+          (helpers.listToUnkeyedAttrs [
+            "prettierd"
+            "prettier"
+            "biome"
+          ])
+          // {
+            stopAfterFirst = true;
+          };
+
+        go = [ "gofmt" ];
+        lua = [ "stylua" ];
+        nix = [ "nixfmt" ];
+        python = [ "black" ];
+        rust = [ "rustfmt" ];
       };
     };
 
-    formattersByFt = {
-      javascript = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
-      typescript = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
-      javascriptreact = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
-      typescriptreact = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
-
-      go = [ "gofmt" ];
-      lua = [ "stylua" ];
-      nix = [ "nixfmt" ];
-      python = [ "black" ];
-      rust = [ "rustfmt" ];
-    };
-
-    formatOnSave = {
-      timeoutMs = 500;
-      lspFallback = true;
-    };
   };
 
   keymaps = [
