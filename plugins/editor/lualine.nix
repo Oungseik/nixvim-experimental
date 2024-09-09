c45d474f0f41f112aa6d33935a3787a685fbef12
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-09-09 09:35:20 +0630

    refactor: migrate to newer configuration of lualine

--- plugins/editor/lualine.nix
+++ plugins/editor/lualine.nix
@@ -2,54 +2,58 @@
   plugins.lualine = {
     enable = true;
 
-    componentSeparators = {
-      right = "";
-      left = "";
-    };
-    sectionSeparators = {
-      left = "";
-      right = "";
-    };
+    settings = {
 
-    globalstatus = true;
-    extensions = [ "fzf" "neo-tree" ];
-    disabledFiletypes = { statusline = [ "startup" "alpha" "NvimTree" ]; };
-    theme = "catppuccin";
+      options = {
+        globalstatus = true;
+        theme = "catppuccin";
 
-    sections = {
-      lualine_a = [
-        {
-          name = "mode";
-          extraConfig = {
-            icon_only = true;
-          };
-        }
+        disabledFiletypes = {
+          statusline = [
+            "startup"
+            "alpha"
+            "NvimTree"
+          ];
+        };
+
+        component_separators = {
+          right = "";
+          left = "";
+        };
+        section_separators = {
+          left = "";
+          right = "";
+        };
+      };
+
+      extensions = [
+        "fzf"
+        "neo-tree"
       ];
-      lualine_b = [
-        {
-          name = "branch";
-          icon = "";
-        }
-      ];
 
-      lualine_c = [
-        {
-          name = "diff";
-          extraConfig = {
+      sections = {
+        lualine_a = [ { __unkeyed-1 = "mode"; } ];
+        lualine_b = [
+          {
+            __unkeyed-1 = "branch";
+            icon = "";
+          }
+        ];
+        lualine_c = [
+          {
+            __unkeyed-1 = "diff";
             symbols = {
               added = " ";
               modified = " ";
               removed = " ";
             };
-          };
-        }
-      ];
+          }
+        ];
 
-      # right 
-      lualine_x = [
-        {
-          name = "diagnostics";
-          extraConfig = {
+        # right 
+        lualine_x = [
+          {
+            __unkeyed-1 = "diagnostics";
             sources = [ "nvim_lsp" ];
             symbols = {
               error = " ";
@@ -57,51 +61,53 @@
               info = " ";
               hint = "󰝶 ";
             };
-          };
-        }
-        { name = "navic"; }
-        {
-          name.__raw = ''
-            function()
-              local icon = " "
-              local status = require("copilot.api").status.data
-              return icon .. (status.message or " ")
-            end,
+          }
+          { __unkeyed-1 = "navic"; }
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                local icon = " "
+                local status = require("copilot.api").status.data
+                return icon .. (status.message or " ")
+              end,
 
-            cond = function()
-             local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
-             return ok and #clients > 0
-            end,
-          '';
-        }
-      ];
+              cond = function()
+               local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+               return ok and #clients > 0
+              end,
+            '';
+          }
+        ];
 
-      lualine_y = [{
-        fmt.__raw = ''
-          function()
-            local buf_clients = vim.lsp.get_clients { bufnr = 0 }
-            if #buf_clients == 0 then
-              return "LSP Inactive"
-            end
+        lualine_y = [
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                local buf_clients = vim.lsp.get_clients { bufnr = 0 }
+                if #buf_clients == 0 then
+                  return "LSP Inactive"
+                end
 
-            local buf_ft = vim.bo.filetype
-            local buf_client_names = {}
+                local buf_ft = vim.bo.filetype
+                local buf_client_names = {}
 
-            -- add client
-            for _, client in pairs(buf_clients) do
-              if client.name ~= "null-ls" and client.name ~= "copilot" then
-                table.insert(buf_client_names, client.name)
-              end
-            end
+                -- add client
+                for _, client in pairs(buf_clients) do
+                  if client.name ~= "null-ls" and client.name ~= "copilot" then
+                    table.insert(buf_client_names, client.name)
+                  end
+                end
 
-            local unique_client_names = table.concat(buf_client_names, ", ")
-            local language_servers = string.format("  %s", unique_client_names)
+                local unique_client_names = table.concat(buf_client_names, ", ")
+                local language_servers = string.format("  %s", unique_client_names)
 
-            return language_servers
-          end,
-        '';
-      }];
-      lualine_z = [{ name = "location"; }];
+                return language_servers
+              end,
+            '';
+          }
+        ];
+        lualine_z = [ { __unkeyed-1 = "location"; } ];
+      };
     };
   };
 }
