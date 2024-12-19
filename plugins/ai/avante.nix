6aa44e19ead9f18c159ac5e03c10a618f039a6af
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-19 13:53:34 +0630

    setup ai with github copilot

---
+++ plugins/ai/avante.nix
@@ -0,0 +1,12 @@
+{
+  plugins.dressing.enable = true;
+  plugins.copilot-lua.enable = true;
+
+  plugins.avante = {
+    enable = true;
+
+    settings = {
+      provider = "copilot";
+    };
+  };
+}
