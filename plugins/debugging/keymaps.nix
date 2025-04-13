6eb1fc27fa53d3807de92ebb1387ed0746342327
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-04-13 13:22:07 +0630

    feat: add key maps for the debugger

--- plugins/debugging/keymaps.nix
+++ plugins/debugging/keymaps.nix
@@ -37,4 +37,78 @@
       desc = "Continue";
     };
   }
+  {
+    mode = "n";
+    key = "<leader>d8";
+    action = ":lua require('dap').step_over()<cr>";
+    options = {
+      silent = true;
+      desc = "Step Over";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>d9";
+    action = ":lua require('dap').step_into()<cr>";
+    options = {
+      silent = true;
+      desc = "Step Into";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>d0";
+    action = ":lua require('dap').step_out()<cr>";
+    options = {
+      silent = true;
+      desc = "Step Out";
+    };
+  }
+
+  # Dap UI keymaps
+  {
+    mode = "n";
+    key = "<leader>du";
+    action = "<Nop>";
+    options = {
+      silent = true;
+      desc = "Dap UI";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>duo";
+    action = ":lua require('dapui').open()<cr>";
+    options = {
+      silent = true;
+      desc = "Open Dap UI";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>duc";
+    action = ":lua require('dapui').close()<cr>";
+    options = {
+      silent = true;
+      desc = "Close Dap UI";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>dol";
+    action = ":lua require('dapui').float_element('scopes', { enter = true })<cr>";
+    options = {
+      silent = true;
+      desc = "Open Locals Float Window";
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>doe";
+    action = ":lua require('dapui').float_element('watches', { enter = true })<cr>";
+    options = {
+      silent = true;
+      desc = "Open Expressions Float Window";
+    };
+  }
 ]
