771629f01443280244bfeeea0e7d8701848489ba
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-04-13 10:26:03 +0630

    refactor: remove plugins need to import from pkgs

--- plugins/utils/default.nix
+++ plugins/utils/default.nix
@@ -1,12 +1,5 @@
 {
-  plugins = {
-    tmux-navigator.enable = true;
-    which-key.enable = true;
-  };
-
   imports = [
-    ./extra.nix
-    # ./flash.nix
     ./harpoon.nix
     ./hurl.nix
     ./mini.nix
