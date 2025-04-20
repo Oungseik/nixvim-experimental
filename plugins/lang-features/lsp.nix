b4637efac483b1fd1d32c10e8d38fbfba571f3b3
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-04-20 16:59:24 +0630

    refactor: remove the usage of deprecated api

--- plugins/lang-features/lsp.nix
+++ plugins/lang-features/lsp.nix
@@ -1,9 +1,15 @@
 {
   extraConfigLuaPre = ''
-    vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn", linehl = "", numhl = "" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint", linehl = "", numhl = "" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo", linehl = "", numhl = "" })
+    vim.diagnostic.config({
+      signs = { 
+        text = { 
+          [vim.diagnostic.severity.ERROR] = "", 
+          [vim.diagnostic.severity.WARN] = "" ,
+          [vim.diagnostic.severity.HINT] = "󰌵", 
+          [vim.diagnostic.severity.INFO] = "", 
+        }, 
+      },
+    })
   '';
 
   diagnostics.virtual_lines = false;
