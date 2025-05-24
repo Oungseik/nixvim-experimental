65c7c108429a1001f3de255c087d7f6d3ab5043b
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-05-24 16:01:34 +0630

    restore nvim-cmp configuration but not enable it

---
+++ plugins/core/cmp-nvim.nix
@@ -0,0 +1,114 @@
+{
+  plugins = {
+    cmp-nvim-lsp.enable = true; # lsp
+    cmp-async-path.enable = true; # file system paths
+    # cmp-buffer = { enable = false; };
+    # cmp-emoji = { enable = false; };
+    # cmp-cmdline = { enable = false; }; # autocomplete for cmdline
+
+    cmp = {
+      enable = true;
+      # lazyLoad = {
+      #   enable = true;
+      #   settings = {
+      #     event = [ "InsertEnter" ];
+      #   };
+      # };
+
+      settings = {
+        autoEnableSources = true;
+        experimental = {
+          ghost_text = true;
+        };
+        performance = {
+          debounce = 60;
+          fetchingTimeout = 200;
+          maxViewEntries = 30;
+        };
+        formatting = {
+          fields = [
+            "abbr"
+            "kind"
+            "menu"
+          ];
+        };
+        sources = [
+          { name = "nvim_lsp"; }
+          # { name = "emoji"; }
+          {
+            name = "buffer"; # text within current buffer
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            keywordLength = 3;
+          }
+          {
+            name = "path"; # file system paths
+            keywordLength = 3;
+          }
+          {
+            name = "lazydev";
+            group_index = 0;
+          }
+        ];
+
+        window = {
+          completion = {
+            border = [
+              "╭"
+              "─"
+              "╮"
+              "│"
+              "╯"
+              "─"
+              "╰"
+              "│"
+            ];
+          };
+          documentation = {
+            border = "solid";
+          };
+        };
+
+        mapping = {
+          "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<C-b>" = "cmp.mapping.scroll_docs(-2)";
+          "<C-f>" = "cmp.mapping.scroll_docs(2)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          # "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        };
+      };
+    };
+  };
+
+  extraConfigLua = ''
+         local cmp = require'cmp'
+
+     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline({'/', "?" }, {
+       sources = {
+         { name = 'buffer' }
+       }
+     })
+
+    -- Set configuration for specific filetype.
+     cmp.setup.filetype('gitcommit', {
+       sources = cmp.config.sources({
+         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+       }, {
+         { name = 'buffer' },
+       })
+     })
+
+     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline(':', {
+       sources = cmp.config.sources({
+         { name = 'path' }
+       }, {
+         { name = 'cmdline' }
+       }),
+     })  '';
+}
