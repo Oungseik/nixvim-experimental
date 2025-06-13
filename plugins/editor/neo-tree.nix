7a21b7a0573250abac7ddc80ba2c76404cbbe5de
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-06-13 10:32:10 +0630

    feat: add git bindings in the neo-tree

--- plugins/editor/neo-tree.nix
+++ plugins/editor/neo-tree.nix
@@ -1,7 +1,18 @@
 {
   plugins.neo-tree = {
     enable = true;
-    window.position = "float";
+    window = {
+      position = "float";
+      mappings = {
+        A = "git_add_all";
+        gu = "git_unstage_file";
+        ga = "git_add_file";
+        gr = "git_revert_file";
+        gc = "git_commit";
+        gp = "git_push";
+        gg = "git_commit_and_push";
+      };
+    };
   };
 
   keymaps = [
